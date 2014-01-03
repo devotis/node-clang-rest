8d72b48dfd5f6416d40681f049aea4b75e6398c2
Christiaan Westerbeek <chris@devotis.nl>
2014-01-03 22:43:33 +0100

    Working Clang REST API
    Example routes:
    /api/clang/customer
    /api/clang/customer/39515
    /api/clang/customer/39515?_method=PUT&firstname=Christiaan
    /api/clang/customer?_method=POST&firstname=test&emailAddress=a@b.nl
    /api/clang/customer/39515?_method=DELETE

--- app.js
+++ app.js
@@ -1,25 +1,79 @@
-var clang = require('clang');
+var express = require('express');
+var clang   = require('clang');
 
+var api;
+var app = express();
+app.all('/api/clang/:object?/:id?', function(req, res) {
+    if (!api) {
+        var uuid = req.headers.uuid || req.query._uuid;
+        if (!uuid || !uuid.match(/^([0-9]-)?[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[89ab][a-f0-9]{3}-[a-f0-9]{12}$/i)) { //Clang (probably) uses a version 4 UUIDs scheme relying only on random numbers.
+            res.status(401); //The client tried to operate on a protected resource without providing the proper authentication credentials.
+            res.send('401 - uuid missing or invalid');
+            return; 
+        }
 
-clang.init('7e5b4df2-7a91-4af0-9119-1fc2d6fdc8a2', '1.18', function(err, api) {
-	if (err) {
-		console.log('err', err.message);	
-	}
+        clang.init(uuid, '1.18', function(err, result) {
+            if (err) {
+                console.log('Error creating clang api', err.message);    
+            } else {
+                console.log('Clang api created');
+                api = result;
+            }
+        });
 
+        res.send('Clang api not created yet. Wait a few seconds and try again.')
+        return;
+    }
 
-	console.log('client', arguments);
+    if (Object.keys(api.objects).indexOf(req.params.object) === -1) {
+    	res.status(404);
+		res.send('404 - resource actually not available');
+		return; 
+    }
 
-	api.objects.group.getAll({}, function(err, rows) {
+    console.log('req.params', req.params);
+    var clangObjectName = req.params.object;
+    var clangMethodName;
+    var args = {};
 
-		console.log('errhere', err, rows);
-
-	});
+    switch(req.query._method || req.method) { //HTTP VERB override through query paramater (override through http header would be better)
+    	case 'GET'   : 
+            if (req.params.id) {
+                clangMethodName = 'getById'
+                args[clangObjectName + 'Id'] = req.params.id;
+            } else if (Object.keys(req.query).length===0) {
+                clangMethodName = 'getAll'
+            } else {
+                clangMethodName = 'getByObject'
+                args[clangObjectName] = req.query;
+            } 
+            break;
+    	case 'POST'  :
+            clangMethodName          = 'insert';
+            args[clangObjectName]    = req.query;
+            break;
+    	case 'PUT'   :
+            clangMethodName          = 'update';
+            args[clangObjectName]    = req.query;
+            args[clangObjectName].id = req.params.id;
+            break;
+    	case 'DELETE':
+            clangMethodName          = 'delete';
+            args[clangObjectName].id = req.params.id;
+            break;
+    	default      :
+            res.status(405); return; //HTTP verb used to access this page is not allowed
+    }
+    
+    api.objects[clangObjectName][clangMethodName](args, function(err, rows) {
+        if (err) {
+            res.status(500);
+            res.send('500 - ' + err.message)
+        } else {
+            res.send(rows);
+        }
+    });
 });
 
-
-/*Interesting
-turns modules into RESTFul web-services: https://npmjs.org/package/webservice
-wrap webservice into functions https://npmjs.org/package/imjs
-
-patterns for modules: http://darrenderidder.github.io/talks/ModulePatterns/#/9
-*/
+app.listen(3000);
+console.log('Listening on port 3000');
