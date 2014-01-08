934b7eecc527481ce1bcccc1844d9fad491f5acb
Christiaan Westerbeek <chris@devotis.nl>
2014-01-08 17:52:26 +0100

    IMprove centralization of error handling
    refs #13

--- app.js
+++ app.js
@@ -9,11 +9,12 @@ var app = express();
 //the sequence of use() matters. It's the sequence middleware is executed
 app.use(express.cookieParser());
 app.use(express.session({secret: config.web.ssecret}));
-app.use(require('express-loggly').requestLogger()); //http://stackoverflow.com/questions/7263626/node-js-how-to-do-something-on-all-http-requests-in-express
+app.use(require('express-loggly').requestLogger());
 app.use(app.router);
 // Since this is the last non-error-handling middleware use()d, we assume 404, as nothing else responded.
 app.use(function(req, res, next){
-    next([404, { status : "error",  error: 'Page not found....' }]);
+    res.statusCode = 404;
+    next(new Error('Page not found.'));
     //pass it on to first piece of error-handling middleware
 });
 
@@ -23,43 +24,39 @@ app.use(function(req, res, next){
 app.use(require('express-loggly').errorLogger()); //http://stackoverflow.com/questions/15684130/express-js-error-handling
 app.use(function(err, req, res, next) {
     console.log('Error handler');
-    if (err) {
-        if (err.length === 2) {
-            res.json(err[0], err[1])
-        } else {
-            res.json(500, { error: err.message });
-        }
-    } else {
-        next();
-    }
+    res.json(500, { status : "error", error: (typeof err === 'string' ? err : err.message) });
 });
 
 app.all('/clang/:object?/:id?/:customaction?', function(req, res, next) {
-    throw new Error('try this errrrrqq123');
+    throw new Error('try this errrrrqq1234');
     
     var uuid = req.headers.uuid || req.query._uuid || req.session.uuid || '';
     //console.log('req.params', req.params);
     if (!uuid || !uuid.match(/^([0-9]-)?[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[89ab][a-f0-9]{3}-[a-f0-9]{12}$/i)) { //Clang (probably) uses a version 4 UUIDs scheme relying only on random numbers.
-        next([401, { status : "error", error: 'uuid missing or invalid (add _uuid=... to your url)' }]); //The client tried to operate on a protected resource without providing the proper authentication credentials.
+        res.statusCode = 401;
+        next(new Error('uuid missing or invalid (add _uuid=... to your url)')); //The client tried to operate on a protected resource without providing the proper authentication credentials.
         return; 
     }
     req.session.uuid = uuid; //store given uuid in session for the browser
 
     if (!api) {
-        next([503, { status : "error", error: 'Clang api not created yet. Try again in a few seconds.' }]);
+        res.statusCode = 503;
+        next(new Error('Clang api not created yet. Try again in a few seconds.'));
         return;
     }
 
     var clangObjectName = req.params.object;
     if (!clangObjectName) {
-        next([500, { status : "error", error: 'Resource not specified' }]);
+        res.statusCode = 500;
+        next(new Error('Resource not specified'));
         return;
     }
     if (clangObjectName.match(/s$/) && !clangObjectName.match(/sms|tatistics$/)) {
         clangObjectName = clangObjectName.slice(0, -1);
     }
     if (Object.keys(api.objects).indexOf(clangObjectName) === -1) {
-        next([404, { status : "error", error: 'Resource ('+clangObjectName+') actually not available' }]);
+        res.statusCode = 404;
+        next(new Error('Resource ('+clangObjectName+') actually not available'));
 		return; 
     }
 
@@ -91,7 +88,8 @@ app.all('/clang/:object?/:id?/:customaction?', function(req, res, next) {
                     args                         = req.query;
                     args[clangObjectName + 'Id'] = req.params.id;
                 } else {
-                    next([500, { status : "error", error: 'Custom action invoked on unspecified resource (use /objects/123/customaction)' }]);
+                    res.statusCode = 500;
+                    next(new Error('Custom action invoked on unspecified resource (use /objects/123/customaction)'));
                     return;
                 }
             } else {
@@ -109,19 +107,22 @@ app.all('/clang/:object?/:id?/:customaction?', function(req, res, next) {
             args.id             = req.params.id;
             break;
     	default      :
-            next([405, { status : "error", error: 'HTTP verb for this resource is not allowed'}]);
+            res.statusCode = 405;
+            next(new Error('HTTP verb for this resource is not allowed'))
             return;
     }
 
     if ( !api.objects[clangObjectName][clangMethodName] ) {
-        next([405, { status : "error", error: 'Method for this resource is not allowed'}]);
+        res.statusCode = 405;
+        next(new Error('Method for this resource is not allowed'));
     }
 
     args.uuid = uuid;
     console.log('xxxxxxxxx')
     api.objects[clangObjectName][clangMethodName](args, function(err, rows) {
         if (err) {
-            next([500, { status : "error", error: err.message }]);
+            res.statusCode = 500;
+            next(err);
         } else {
             res.json({status : "success", data : rows});
         }
