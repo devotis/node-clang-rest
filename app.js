d7ef3f507d9826e1e801d8eebe2b0c0ce645dabc
Christiaan Westerbeek <chris@devotis.nl>
2015-12-04 17:54:36 +0100

    Use configured port

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ app.all('/:object/:id?/:customaction?', function(req, res) {
   });
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
 
   var host = server.address().address;
   var port = server.address().port;
