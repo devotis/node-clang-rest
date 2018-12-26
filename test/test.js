379b675ec4d8aa21129a7b73d3085172c71c3c6b
Christiaan Westerbeek <chris@devotis.nl>
2018-12-26 13:23:38 +0100

    obsolete tests

--- test/test.js
+++
@@ -1,6 +0,0 @@
-//Get web.config variables into process.env
-require('llips')().test.webconfig();
-process.env['mocha-unfunk-style'] = 'css';
-
-describe('Brands', require('./suites/brands'));
-describe('Customers', require('./suites/customers'));
